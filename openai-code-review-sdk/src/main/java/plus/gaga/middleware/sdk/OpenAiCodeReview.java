aeb555a8aafdf8d10250bb1b597d62fcbd6fe556
opwbkf111 <842123094@qq.com>
2024-12-26 16:08:43 +0800

    feat:检出代码

--- openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
@@ -1,10 +1,29 @@
 package plus.gaga.middleware.sdk;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class OpenAiCodeReview {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("呵呵哈哈哈或或11");
-        System.out.println("呵呵哈哈哈或或22");
+
+        ProcessBuilder processBuilder = new ProcessBuilder("git","diff","HEAD~1","HEAD");
+        processBuilder.directory(new File("."));
+
+        Process process = processBuilder.start();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+        String line;
+        StringBuilder differCode = new StringBuilder();
+        while((line=reader.readLine())!=null){
+            differCode.append(line);
+        }
+        int exitCode = process.waitFor();
+        System.out.println("Exit with code"+exitCode);
+        System.out.println("评审代码"+differCode.toString());
     }
 
 }
