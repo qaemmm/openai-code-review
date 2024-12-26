d98e2053fa3500d78f06b82e4a279aa79c09e613
opwbkf111 <842123094@qq.com>
2024-12-26 15:07:32 +0800

    测试打印消息

--- openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
@@ -3,7 +3,8 @@ package plus.gaga.middleware.sdk;
 public class OpenAiCodeReview {
 
     public static void main(String[] args) {
-        System.out.println("测试执行");
+        System.out.println("呵呵哈哈哈或或11");
+        System.out.println("呵呵哈哈哈或或22");
     }
 
 }
