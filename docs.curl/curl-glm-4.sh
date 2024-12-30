35bae6ff5efc3e2cf14d918fd556aea52bfdb247
86130 <842123094@qq.com>
2024-12-30 22:09:12 +0800

    feat: 第7节：工程重构，增加可维护性 V7.7

--- docs.curl/curl-glm-4.sh
+++ docs.curl/curl-glm-4.sh
@@ -1,8 +1,8 @@
 curl --location 'https://open.bigmodel.cn/api/paas/v4/chat/completions' \
---header 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsInNpZ25fdHlwZSI6IlNJR04ifQ.eyJhcGlfa2V5IjoiZGNhY2RkNWY2OTgyNGY0OTA5ZTI3OGNiM2FjMTc1YTEiLCJleHAiOjE3MzU1NDEzMTUxMzYsInRpbWVzdGFtcCI6MTczNTUzOTUxNTEzN30.Ue_K1BuMGazgekVu6JUYhYUZRpAR5YUXFIyQaWzuSMc' \
+--header 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsInNpZ25fdHlwZSI6IlNJR04ifQ.eyJhcGlfa2V5IjoiZGNhY2RkNWY2OTgyNGY0OTA5ZTI3OGNiM2FjMTc1YTEiLCJleHAiOjE3MzU1Njg5ODkyMTYsInRpbWVzdGFtcCI6MTczNTU2NzE4OTIxN30.y4OushUOPEtGkdj5tcdTwIUsJ76r9A0ZTB7nrlFUDzQ' \
 --header 'Content-Type: application/json' \
 --data '{
-    "model": "glm-4",
+    "model": "glm-4-flash",
     "messages": [
         {
             "role": "user",
