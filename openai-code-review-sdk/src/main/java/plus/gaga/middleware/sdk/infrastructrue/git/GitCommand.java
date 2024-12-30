dce384afb0dca6acf298e923f0e08b3dc39e5977
86130 <842123094@qq.com>
2024-12-30 21:53:35 +0800

    feat: 第7节：工程重构，增加可维护性 V7.6

--- openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/infrastructrue/git/GitCommand.java
+++ openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/infrastructrue/git/GitCommand.java
@@ -40,32 +40,36 @@ public class GitCommand {
     }
     //查看当前不同的
     public String diff()throws Exception{
-        ProcessBuilder logProcessBuilder = new ProcessBuilder("git","diff","-1","--pretty=format:%H");
+        // openai.itedus.cn
+        ProcessBuilder logProcessBuilder = new ProcessBuilder("git", "log", "-1", "--pretty=format:%H");
+        logProcessBuilder.directory(new File("."));
         Process logProcess = logProcessBuilder.start();
-        //去查看提交记录不一样的东西
+
         BufferedReader logReader = new BufferedReader(new InputStreamReader(logProcess.getInputStream()));
-        String lastCommitHash = logReader.readLine();
+        String latestCommitHash = logReader.readLine();
         logReader.close();
         logProcess.waitFor();
-        ProcessBuilder diffProcessBuilder = new ProcessBuilder("git","diff",lastCommitHash+"^",lastCommitHash);
+
+        ProcessBuilder diffProcessBuilder = new ProcessBuilder("git", "diff", latestCommitHash + "^", latestCommitHash);
+        diffProcessBuilder.directory(new File("."));
         Process diffProcess = diffProcessBuilder.start();
-        //现在就是读取到了diff的差异化数据
-        StringBuilder sb = new StringBuilder();
+
+        StringBuilder diffCode = new StringBuilder();
         BufferedReader diffReader = new BufferedReader(new InputStreamReader(diffProcess.getInputStream()));
-        String inputLine ;
-        while((inputLine=diffReader.readLine())!=null){
-            sb.append(inputLine);
+        String line;
+        while ((line = diffReader.readLine()) != null) {
+            diffCode.append(line).append("\n");
         }
         diffReader.close();
-        int diffCode = diffProcess.waitFor();
 
-        if(diffCode!=0){
-            throw new RuntimeException("diff命令执行失败");
+        int exitCode = diffProcess.waitFor();
+        if (exitCode != 0) {
+            throw new RuntimeException("Failed to get diff, exit code:" + exitCode);
         }
 
-
-        return sb.toString();
+        return diffCode.toString();
     }
+
 
     public String commitAndPush(String recommend)throws Exception{
         //通过java来操作git命令完成commit和push操作
